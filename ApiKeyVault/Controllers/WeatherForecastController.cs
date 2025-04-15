d21d9aa7afb9aab993b2cec3082710fdd5291241
Siddhesh Prabhugaonkar <siddheshpg@gmail.com>
2025-04-15 13:51:54 +0530

    Add project files.

---
+++ ApiKeyVault/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiKeyVault.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("keyvault")]
+        public IActionResult GetSecret()
+        {
+            string keyVaultUrl = "https://kvdemo87877.vault.azure.net/";
+            var client = new SecretClient(new Uri(keyVaultUrl), 
+                new DefaultAzureCredential());
+            KeyVaultSecret secret = client.GetSecret("ApiKey");
+            string secretValue = secret.Value;
+            return Ok(secretValue);
+        }
+    }
+}
